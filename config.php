24acb90e643feb8fe9a3ac6722f444fbcb46cdf7
rahmonaliulciev39-web <rahmonaliulciev39@gmail.com>
2026-05-16 14:39:24 +0500

    Add configuration settings and API keys

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+// Твои настройки доступа и ключи
+$admin = "721324878";
+$token = "8576067372:AAGeCbsX2kZnHho0wnzkKZJUBLx7BHoeSQk";
+$url = "https://api.telegram.org/bot$token/";
+
+// API Ключи для бирж
+$bx_k = "GMEI6bgybO5yIOnrOykMT1d4FoJAo15g1jwrERq7rkRUTDkd3IcFURGEidz9lFdYoyQVCgZfQj0OxvbSx8pVg";
+$bx_s = "ryqLZUnLw9kCF7cfXyyxu9EjYilC4KBYbLE0npoiM3yU7XCQjF5T5oYMjBq5eF82XE5bwwaNFRPMUE6w";
+$mx_k = "mx0vgl3WcKpWFFOplO";
+$mx_s = "d36fda3bea424c6badb41e60dc901b78";
+?>
